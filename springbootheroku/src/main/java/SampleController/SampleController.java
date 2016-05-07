97935ee305827b8c4ed61aacbf634c605b20e3a3
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2016-05-06 23:20:54 -0500

    deploy spring boot on heroku

---
+++ springbootheroku/src/main/java/SampleController/SampleController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package SampleController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author eratnch
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Heroku From Sample Spring-Boot";
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 SpringApplication.run(SampleController.class, args);
+	}
+
+}
